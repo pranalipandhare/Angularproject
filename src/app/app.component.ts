fdca99feef6d6ed207d403b3fa7613b1b22e9fd5
Pranalipandhare <pranalipandhare2002@gmail.com>
2024-08-05 23:32:17 +0530

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +9,10 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'angularproject';
 
+num:number=23;
+num1:number=45.67;
+num2=456;
+arr1 = [12, "c", 23.4];
 courses = ["C", "C++", "Java"]
 arr  = [2, "c", 100, 3]
 
@@ -17,5 +22,47 @@ student = {
   city: "Pune",
   marks: 90
 }
+
+showmsg(){
+  console.log("Hello Students")
+}
+
+Employees=[
+  {
+   ename: "John",
+   sal:34567,
+   Country:"US",
+   deptno:20,
+   Role:"Admin" 
+  },
+  {
+    ename: "Nlake",
+    sal:34007,
+    Country:"UK",
+    deptno:38,
+    Role:"Developer" 
+   },
+   {
+    ename: "Rohit",
+    sal:34000,
+    Country:"India",
+    deptno:21,
+    Role:"Manager" 
+   },
+   {
+    ename: "Scott",
+    sal:45000,
+    Country:"Germany",
+    deptno:80,
+    Role:"Salesma" 
+   },
+   {
+    ename: "Vishal",
+    sal:30000,
+    Country:"India",
+    deptno:41,
+    Role:"Developer" 
+   }
+]
 }
 
